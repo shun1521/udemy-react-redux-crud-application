fe3acfd1c0cf1146085f6e869977b72ebf49bd50
arai_shunta <arai.tohoku@icloud.com>
2021-11-28 20:59:16 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React,{Component} from "react";
-import PropTypes from "prop-types";
-//class App extends Component {
-  //render(){
-    //return (
-  //  <React.Fragment>
-  //  <label htmlFor="bar">bar</label>
-  ///  <input type = "text" onChange = {()=>{console.log("I am clicked !")}}/>
-  ///  </React.Fragment>
-//  )
-//  }
-//}
 
-const User = (props)=>{
-  return (
-    <div>
-    Hi, I am {props.name}! an {props.age} years old!
-    </div>
-  )
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = {count:0}
+  }
+  handlePlusButton = ()=>{
+    console.log("handlePlusButon")
+    this.setState({count:this.state.count + 1})
+  }
+  handleMinusButton = ()=>{
+    console.log("handleMinusButon")
+    this.setState({count:this.state.count - 1})
+  }
+  render(){
+    return (
+      <React.Fragment>
+      <div>count: {this.state.count }</div>
+      <button onClick = {this.handlePlusButton}>+1</button>
+      <button onClick = {this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
-//プロパティの型はあらかじめ決めておく。
-User.propTypes = {
-name : PropTypes.string,
-age: PropTypes.number.isRequired
-}
-
 
 const App = ()=>{
-  const profiles = [
-    {name: "Taro",age:10},
-    {name: "Hanko",age:6},
-    {name:"Noname",}
-  ]
   return (
-    <div>
-    {
-    profiles.map((profile,index) => {
-      return <User name={profile.name} age =  {profile.age} key = {index}/>
-})}
-  </div>
+    <Counter></Counter>
   )
 }
 export default App;
