eacc03161cd8118ea294c48fd63678220c72f8e9
arai_shunta <arai.tohoku@icloud.com>
2021-11-28 20:16:13 +0900

    create class components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React,{Component} from "react";
 
-class App extends Component {
-  render(){
-    return (
-    <React.Fragment>
-    <label htmlFor="bar">bar</label>
-    <input type = "text" onChange = {()=>{console.log("I am clicked !")}}/>
-    </React.Fragment>
+//class App extends Component {
+  //render(){
+    //return (
+  //  <React.Fragment>
+  //  <label htmlFor="bar">bar</label>
+  ///  <input type = "text" onChange = {()=>{console.log("I am clicked !")}}/>
+  ///  </React.Fragment>
+//  )
+//  }
+//}
+
+const Cat = ()=>{
+  return (
+    <div>
+    Meow!
+    </div>
   )
-  }
+}
+const App = ()=>{
+  return (
+    <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    </div>
+  )
 }
 export default App;
